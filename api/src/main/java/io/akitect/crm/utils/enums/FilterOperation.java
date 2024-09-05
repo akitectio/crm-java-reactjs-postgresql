8336e74833bdb894c559636a38d96b6e755d1b21
ANTL3 <lapan.truong@gmail.com>
2024-09-05 20:45:52 +0700

    updated: Filter Operation enum

--- api/src/main/java/io/akitect/crm/utils/enums/FilterOperation.java
+++ api/src/main/java/io/akitect/crm/utils/enums/FilterOperation.java
@@ -1,16 +1,6 @@
-package io.akitect.crm.utils.constants;
+package io.akitect.crm.utils.enums;
 
 public enum FilterOperation {
-    // public final String EQUAL = "=";
-    // public final String NOT_EQUAL = "!=";
-    // public final String LESS_THAN = "<";
-    // public final String LESS_OR_EQUAL = "<=";
-    // public final String MORE_THAN = ">";
-    // public final String MORE_OR_EQUAL = ">=";
-    // public final String IS = "is";
-    // public final String IS_NOT = "is not";
-    // public final String IN = "in";
-
     EQUAL("="),
     NOT_EQUAL("!="),
     LESS_THAN("<"),
